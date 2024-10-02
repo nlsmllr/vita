dbd2554038e6ebea3eabd589577d8582b3182d1e
NMUProtofy <nils.mueller@protofy.com>
2024-10-02 11:39:51 +0200

    fix: mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function Developer() {
   return (
-    <main className="mx-auto flex h-screen flex-col items-center justify-evenly uppercase text-black">
+    <main className="mx-auto flex h-screen flex-col items-center justify-center uppercase text-black sm:justify-evenly">
       <Link href={'/'}>
         <h1 className="text-center text-5xl font-black tracking-wide sm:text-9xl">Nils Müller</h1>
       </Link>
-      <div className="flex flex-col space-x-20 text-3xl font-bold tracking-wide sm:flex-row sm:text-4xl">
+      <div className="my-7 flex flex-col space-x-[70px] text-3xl font-bold tracking-wide sm:my-0 sm:flex-row sm:text-4xl">
         <Link className="md:hover:blur-sm" href={'/dev'}>
           Developer
         </Link>
