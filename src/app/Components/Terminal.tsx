44ad32c3c875c01ca831805d37dd36e7fb58c383
NMUProtofy <nils.mueller@protofy.com>
2024-10-02 13:43:54 +0200

    fix: no zoom on mobile

--- src/app/Components/Terminal.tsx
+++ src/app/Components/Terminal.tsx
@@ -77,7 +77,7 @@ export const Terminal = () => {
                   handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
                 }
               }}
-              className="touch-action-manipulation h-48 w-full resize-none items-start border-none bg-transparent font-mono outline-none" // Added touch-action and font-size
+              className="touch-action-manipulation h-48 w-full resize-none items-start border-none bg-transparent font-mono outline-none"
               autoFocus
             />
           </label>
