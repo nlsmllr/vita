eb73e12e163812ea160105968a22028d4d3b456f
nlsmllr <info@nils-mueller.com>
2024-10-09 10:36:03 +0200

    fix: blur on contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,10 +13,10 @@ export default function Photographer() {
         <h1 className="text-center text-5xl font-black tracking-wide sm:text-9xl">Nils Müller</h1>
       </Link>
       <div className="my-7 flex cursor-none flex-col text-center text-3xl font-bold tracking-wide sm:my-0 sm:flex-row sm:space-x-[30px] sm:text-4xl">
-        <Link href="mailto:info@nils-mueller.com" className="link cursor-none">
+        <Link href="mailto:info@nils-mueller.com" className="link cursor-none md:hover:blur-sm">
           info@nils-mueller.com
         </Link>
-        <Link href="tel:+4915115776494" className="link cursor-none">
+        <Link href="tel:+4915115776494" className="link cursor-none md:hover:blur-sm">
           +49 151 15776494
         </Link>
       </div>
