85d7cdc269d9be9bc84506e063b6cc2638c0916b
nlsmllr <info@nils-mueller.com>
2024-10-10 14:22:11 +0200

    fix: CV

--- src/app/ComponentsDev/Window.tsx
+++ src/app/ComponentsDev/Window.tsx
@@ -27,7 +27,7 @@ export const Window = ({
       onClick={isMinimized ? onMinimize : undefined}
     >
       <div className="overflow-hidden rounded-lg bg-zinc-950 shadow-xl sm:h-full">
-        <div className="flex items-center rounded-t-lg bg-zinc-800 px-4 py-2">
+        <div className="topbar sticky top-0 flex items-center overflow-auto rounded-t-lg bg-zinc-800 px-4 py-2">
           <div className="flex space-x-2 pr-3 sm:pr-0">
             <div className="h-3 w-3 cursor-pointer rounded-full bg-red-500 hover:bg-red-600" onClick={onClose}></div>
             <div
