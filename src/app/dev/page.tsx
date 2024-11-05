d8e41bfc3b08a7f0ae62b1d233a5799473154317
nlsmllr <info@nils-mueller.com>
2024-11-05 15:40:40 +0100

    height reduced on page

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   };
 
   return (
-    <div className="relative h-[90vh] overflow-hidden scroll-auto">
+    <div className="relative h-[80vh] overflow-hidden scroll-auto">
       <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center font-bold text-zinc-800">
         <h1 className="text-5xl">Oops!</h1>
         {!isVisible && (
