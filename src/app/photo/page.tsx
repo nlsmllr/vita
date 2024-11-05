86b0de4c9b5aee9cf1a44f43164972bd3ec24f04
nlsmllr <info@nils-mueller.com>
2024-11-05 16:48:35 +0100

    link photos

--- src/app/photo/page.tsx
+++ src/app/photo/page.tsx
@@ -1,21 +1,59 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { ContactButton } from '../ComponentsPhoto/ContactButton';
 import CustomCursor from '../ComponentsPhoto/CustomCursor';
+import { imageFilenames } from '../Constants/photos';
 
-export default function Photographer() {
+export const dynamic = 'force-dynamic';
+
+export default function Photo() {
   return (
-    <main className="mx-auto mt-72 flex h-auto cursor-none flex-col items-center justify-center uppercase text-black sm:mt-0 sm:h-screen sm:justify-evenly">
+    <div className="relative h-auto cursor-none bg-white text-black">
       <CustomCursor />
       <ContactButton link={'contact'} />
-      <Link className="cursor-none" href={'/'}>
-        <h1 className="text-center text-5xl font-black tracking-wide sm:text-9xl">Nils Müller</h1>
-      </Link>
-      <div className="my-7 flex cursor-none flex-col space-x-[70px] text-3xl font-bold tracking-wide sm:my-0 sm:flex-row sm:text-4xl">
-        <Link className="cursor-none md:blur-sm md:transition-all md:duration-300 md:hover:blur-0" href={'/'}>
-          Coming soon 💦
-        </Link>
+      <div className="flex h-[75vh] w-screen items-end justify-center">
+        <div className="text-md z-10 font-light uppercase tracking-widest text-zinc-950">scroll down</div>
       </div>
-    </main>
+      <div
+        className="fixed left-0 top-0 z-10 h-[74vh] w-full"
+        style={{
+          backgroundImage: 'linear-gradient(to bottom, white, white 74%, transparent 100%)',
+        }}
+      ></div>
+      <section className="fixed inset-0 z-10 flex h-screen items-center justify-center">
+        <Link
+          className="mx-auto flex h-full cursor-none flex-col items-center justify-center uppercase text-black"
+          href={'/'}
+        >
+          <h1 className="-mt-10 text-center text-5xl font-black tracking-wide sm:text-9xl">Nils Müller</h1>
+        </Link>
+      </section>
+      <section className="relative z-20 mt-[1000px] cursor-none sm:mx-40">
+        <div className="grid grid-cols-1 gap-4 px-12 sm:p-4 md:grid-cols-4">
+          {imageFilenames.map((filename, index) => (
+            <div
+              key={index}
+              className="relative col-span-2"
+              style={{
+                marginTop:
+                  index % 2 === 0 ? `max(20px, ${Math.random() * 200}px)` : `max(50px, ${Math.random() * 200 + 50}px)`,
+                marginLeft: '-10%',
+                zIndex: imageFilenames.length - index,
+              }}
+            >
+              <Image
+                src={`/images/${filename}`}
+                alt={`Image ${index + 1}`}
+                width={2500}
+                height={2500}
+                className="w-full object-cover transition md:duration-200"
+              />
+              <span className="absolute -right-8 top-0 text-xl text-red-500">{String(index + 1).padStart(2, '0')}</span>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
---
+++ src/app/photography-no-longer-active/page.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+import { ContactButton } from '../ComponentsPhoto/ContactButton';
+import CustomCursor from '../ComponentsPhoto/CustomCursor';
+
+export default function Photographer() {
+  return (
+    <main className="mx-auto mt-72 flex h-auto cursor-none flex-col items-center justify-center uppercase text-black sm:mt-0 sm:h-screen sm:justify-evenly">
+      <CustomCursor />
+      <ContactButton link={'contact'} />
+      <Link className="cursor-none" href={'/'}>
+        <h1 className="text-center text-5xl font-black tracking-wide sm:text-9xl">Nils Müller</h1>
+      </Link>
+      <div className="my-7 flex cursor-none flex-col space-x-[70px] text-3xl font-bold tracking-wide sm:my-0 sm:flex-row sm:text-4xl">
+        <Link className="cursor-none md:blur-sm md:transition-all md:duration-300 md:hover:blur-0" href={'/'}>
+          Coming soon 💦
+        </Link>
+      </div>
+    </main>
+  );
+}
