b408a0998477265380e2af8c5710e68b5642277c
NMUProtofy <nils.mueller@protofy.com>
2024-11-08 10:01:09 +0100

    color

--- src/app/ComponentsPhoto/ContactButton.tsx
+++ src/app/ComponentsPhoto/ContactButton.tsx
@@ -6,7 +6,7 @@ interface ContactButtonProps {
 
 export const ContactButton: React.FC<ContactButtonProps> = ({ link }) => (
   <Link href={`/${link}`} className="cursor-none">
-    <div className="fixed -right-10 -top-10 z-30 transition duration-100 hover:blur-sm">
+    <div className="fixed -right-10 -top-10 z-30 transition duration-100 sm:hover:blur-sm">
       <div className="dotPulse flex h-20 w-20 items-center justify-center font-bold text-white"></div>
     </div>
   </Link>
