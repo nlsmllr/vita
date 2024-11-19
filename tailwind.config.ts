b458d2fd6205f59dde48b62f231538db739d0554
NMUProtofy <nils.mueller@protofy.com>
2024-11-19 10:34:38 +0100

    sample colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,5 +2,15 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-bg': {
+          light: '#DEDBD2',
+          dark: '#A8A076',
+        },
+      },
+    },
+  },
 };
 export default config;
