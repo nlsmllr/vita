67b8f8b9178a24232b0ac06ec43390b4f3f83934
NMUProtofy <nils.mueller@protofy.com>
2024-11-27 10:55:02 +0100

    feat: cursor scale

--- src/app/ComponentsPhoto/CustomCursor.tsx
+++ src/app/ComponentsPhoto/CustomCursor.tsx
@@ -1,4 +1,3 @@
-// CustomCursor.tsx
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
@@ -6,6 +5,7 @@ import { useEffect, useRef, useState } from 'react';
 export default function CustomCursor() {
   const cursorRef = useRef<HTMLDivElement>(null);
   const [isCursorVisible, setIsCursorVisible] = useState(false);
+  const [isHoveringLink, setIsHoveringLink] = useState(false);
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
@@ -14,25 +14,37 @@ export default function CustomCursor() {
       }
 
       if (cursorRef.current) {
-        cursorRef.current.style.left = `${event.clientX - 20}px`;
-        cursorRef.current.style.top = `${event.clientY - 20}px`;
+        cursorRef.current.style.left = `${event.clientX}px`;
+        cursorRef.current.style.top = `${event.clientY}px`;
       }
     };
 
+    const handleMouseEnterLink = () => setIsHoveringLink(true);
+    const handleMouseLeaveLink = () => setIsHoveringLink(false);
+
     document.addEventListener('mousemove', handleMouseMove);
+    document.querySelectorAll('a').forEach(link => {
+      link.addEventListener('mouseenter', handleMouseEnterLink);
+      link.addEventListener('mouseleave', handleMouseLeaveLink);
+    });
 
     return () => {
       document.removeEventListener('mousemove', handleMouseMove);
+      document.querySelectorAll('a').forEach(link => {
+        link.removeEventListener('mouseenter', handleMouseEnterLink);
+        link.removeEventListener('mouseleave', handleMouseLeaveLink);
+      });
     };
   }, [isCursorVisible]);
 
   return (
     <div
       ref={cursorRef}
-      className={`pointer-events-none fixed z-50 h-10 w-10 cursor-none rounded-full mix-blend-difference transition-opacity ${
+      className={`pointer-events-none fixed z-50 rounded-full mix-blend-difference transition-[width,height] duration-500 ease-in-out ${
         isCursorVisible ? 'opacity-100' : 'opacity-0'
-      }`}
+      } ${isHoveringLink ? 'h-16 w-16' : 'h-10 w-10'}`}
       style={{
+        transform: 'translate(-50%, -50%)',
         background: 'white',
         mixBlendMode: 'difference',
       }}
