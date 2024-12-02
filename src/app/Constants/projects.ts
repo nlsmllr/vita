7d67ea7159d1bc704d056c0075887bc9a6ebed77
nlsmllr <info@nils-mueller.com>
2024-12-02 12:27:31 +0100

    vita changes

--- src/app/Constants/projects.ts
+++ src/app/Constants/projects.ts
@@ -2,8 +2,12 @@
 
 export const projects = [
   { text: 'Huffman encoding using C', link: '' },
-  { text: 'Collatz conjecture calculation and visualization using Python', link: '' },
+  {
+    text: 'Collatz conjecture calculation and visualization using Next.js TailwindCSS, Vercel',
+    link: 'https://collatz-conjecture-ten.vercel.app/',
+  },
   { text: 'Image processing using C++', link: '' },
-  { text: 'Smart Cities Dashboard using Next.js, Tailwind, Vercel, Spring', link: 'https://scfe.vercel.app/' },
-  { text: 'Connections Game', link: 'https://connectionsgame.vercel.app/' },
+  { text: 'Creating a VR experience unsing C#', link: '' },
+  { text: 'Smart Cities Dashboard using Next.js TailwindCSS, Vercel, Spring', link: 'https://scfe.vercel.app/' },
+  { text: 'Connections Game using Next.js TailwindCSS, Vercel', link: 'https://connectionsgame.vercel.app/' },
 ];
