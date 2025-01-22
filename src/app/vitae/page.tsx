c6ad8520aa9d67b06fc724c258e59526cc4daefc
NMUProtofy <nils.mueller@protofy.com>
2025-01-22 13:28:40 +0100

    fix: width

--- src/app/vitae/page.tsx
+++ src/app/vitae/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <div className="flex h-full w-screen cursor-none justify-center">
       <CustomCursor />
-      <div className="mb-20 w-1/2 cursor-none">
+      <div className="mb-20 w-4/5 cursor-none sm:w-1/2">
         <Content color="black" />
       </div>
     </div>
