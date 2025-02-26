283676312ca11520e5fda2ee735adf5d5b064d72
nlsmllr <info@nils-mueller.com>
2025-02-26 20:36:45 +0100

    feat: transparent terminal

--- src/app/ComponentsDev/Window.tsx
+++ src/app/ComponentsDev/Window.tsx
@@ -26,7 +26,8 @@ export const Window = ({
       }}
       onClick={isMinimized ? onMinimize : undefined}
     >
-      <div className="overflow-hidden rounded-lg bg-zinc-950 shadow-xl sm:h-full">
+      <div className="overflow-hidden rounded-lg shadow-xl sm:h-full">
+        {/* Top Bar */}
         <div className="topbar sticky top-0 flex items-center overflow-auto rounded-t-lg bg-zinc-800 px-4 py-2">
           <div className="flex space-x-2 pr-3 sm:pr-0">
             <div className="h-3 w-3 cursor-pointer rounded-full bg-red-500 hover:bg-red-600" onClick={onClose}></div>
@@ -42,7 +43,7 @@ export const Window = ({
             <span className="text-sm text-zinc-400">{title}</span>
           </div>
         </div>
-        <div className="scrollbar-hide h-full overflow-auto rounded-b-lg font-mono text-sm text-gray-300">
+        <div className="h-full overflow-auto rounded-b-lg bg-zinc-950/25 p-5 font-mono text-sm text-gray-300 backdrop-blur-lg">
           {children}
         </div>
       </div>
