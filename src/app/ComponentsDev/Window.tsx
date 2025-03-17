7bd2d04855ac5077b7a17baf944ac8b86614dfd3
nlsmllr <info@nils-mueller.com>
2025-03-17 09:40:24 +0100

    fix: a11y

--- src/app/ComponentsDev/Window.tsx
+++ src/app/ComponentsDev/Window.tsx
@@ -37,14 +37,19 @@ export const Window = ({ children, onClose, onMinimize, isMinimized = false }: W
       <div className="overflow-hidden rounded-lg bg-zinc-950 shadow-xl sm:h-full">
         <div className="topbar sticky top-0 flex items-center overflow-auto rounded-t-lg bg-zinc-800 px-4 py-2">
           <div className="flex space-x-2 pr-3 sm:pr-0">
-            <div className="h-3 w-3 cursor-pointer rounded-full bg-red-500 hover:bg-red-600" onClick={onClose}></div>
-            <div
+            <button
+              className="h-3 w-3 cursor-pointer rounded-full bg-red-500 hover:bg-red-600"
+              onClick={onClose}
+            ></button>
+            <button
               className="h-3 w-3 cursor-pointer rounded-full bg-yellow-500 hover:bg-yellow-600"
               onClick={onMinimize}
-            ></div>
-            <Link href={'/contact'}>
-              <div className="h-3 w-3 rounded-full bg-green-500 hover:bg-green-600"></div>
-            </Link>
+            ></button>
+            <Link
+              href={'/contact'}
+              aria-label="Button leading to homepage"
+              className="h-3 w-3 rounded-full bg-green-500 hover:bg-green-600"
+            ></Link>
           </div>
           <div className="flex-grow text-center">
             <span className="text-sm text-zinc-400">{windowTitle}</span>
