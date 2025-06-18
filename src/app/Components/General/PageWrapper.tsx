400ab6156d30ce9ae241c4f788d5b164c035fa6c
Nils Müller <75166888+nlsmllr@users.noreply.github.com>
2025-06-18 14:22:04 +0200

    Update PageWrapper.tsx

--- src/app/Components/General/PageWrapper.tsx
+++ src/app/Components/General/PageWrapper.tsx
@@ -1,25 +1,25 @@
-'use client';
+// 'use client';
 
-import { usePathname } from 'next/navigation';
-import React from 'react';
+// import { usePathname } from 'next/navigation';
+// import React from 'react';
 
-import BackgroundFrame from './BackgroundFrame';
-import Navigation from './Navigation';
+// import BackgroundFrame from './BackgroundFrame';
+// import Navigation from './Navigation';
 
-interface PageWrapperProps {
-  children: React.ReactNode;
-}
+// interface PageWrapperProps {
+//   children: React.ReactNode;
+// }
 
-const PageWrapper: React.FC<PageWrapperProps> = ({ children }) => {
-  const pathname = usePathname();
-  const isPhotoPage = pathname === '/dev';
+// const PageWrapper: React.FC<PageWrapperProps> = ({ children }) => {
+//   const pathname = usePathname();
+//   const isPhotoPage = pathname === '/dev';
 
-  return (
-    <BackgroundFrame inverted={isPhotoPage}>
-      <Navigation inverted={isPhotoPage} />
-      <div className="flex-1">{children}</div>
-    </BackgroundFrame>
-  );
-};
+//   return (
+//     <BackgroundFrame inverted={isPhotoPage}>
+//       <Navigation inverted={isPhotoPage} />
+//       <div className="flex-1">{children}</div>
+//     </BackgroundFrame>
+//   );
+// };
 
-export default PageWrapper;
+// export default PageWrapper;
